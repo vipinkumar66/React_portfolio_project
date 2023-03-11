70dd1e91edbd249f2bf2475c063e14a9420be038
vipin <vipinkumarlamba55@gmail.com>
2023-03-12 02:10:40 +0530

    modified about container

--- src/containers/about/About.jsx
+++ src/containers/about/About.jsx
@@ -4,37 +4,6 @@ import Aos from 'aos'
 import 'aos/dist/aos.css'
 import { FaGithub, FaLinkedin, FaInstagram, FaYoutube } from 'react-icons/fa';
 
-const Accordion = ({heading1, heading2, heading3, text1, text2, text3}) =>{
-  return(
-    <div class="accordion" id="accordionPanelsStayOpenExample">
-              <div class="accordion-item">
-                <h2 class="accordion-header" id="panelsStayOpen-headingOne">
-                  <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-                    <h5>{heading1}</h5>
-                  </button>
-                </h2>
-                <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
-                  <div class="accordion-body">
-                    <p>{text1}</p> 
-                  </div>
-                </div>
-              </div>
-              <div class="accordion-item">
-                <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
-                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
-                    <h5>{heading2}</h5>
-                  </button>
-                </h2>
-                <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
-                  <div class="accordion-body">
-                    <p>{text2}</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-  );
-}
 
 const About = () => {
 
@@ -43,9 +12,9 @@ const About = () => {
   },[])
 
   return (
-    <div>
+    <div className='about'>
       <div className='container about__container'>
-        <div className='row about__container-intro '>
+        <div className='row about__container-intro mb-5'>
           <div className='col-sm-6'>
             <img src="images/hello.svg" alt="hello" className='img-fluid'/>
           </div>
@@ -56,36 +25,33 @@ const About = () => {
         </div>
       </div>
       <div className='container about__container-graduation mt-5 mb-5' data-aos="fade-right">
-        <div className='row'>
-          {/* <div className='col-sm-5'> */}
-          <h3>EDUCATION</h3>
-          <Accordion 
-            heading1="Higher Secondary Education"
-            text1=""
-            heading2="Graduation"
-            text2=""
-          />
+        <h1>Education</h1>
+        <div className='about__container-graduation__small'>
+          <h3>Higher Secondary Education</h3>
+          <p>I completed my 11th and 12th from APS Bathinda, Punjab and KV Bathinds, Punjab respectively with 70%. My stream in 12th was PCM (physics/chemistry/mathematics).</p>
+          <h3>Graduation</h3>
+          <p>I completed my BE (Bachelors of Engineering) in Electronics and communication form Vemana Institute of Technology, Bangalore with 5.8 cgpa.</p>
         </div>
       </div>
-      <div className='container about__container-socialmedia g-2 mt-5' data-aos="fade-left">
-        <div className='row'>
-          <div className='col-sm-7'>
-            <img src="images/social_media.svg" alt="socialmedia" className='img-fluid'/>
-          </div>
-          <div className='col-sm-4 my-auto text-center'>
-            <h3 className='fst-italic'>Stay connected with me on <strong className='text-danger'>social media </strong>...</h3>
-            <div className="card skills-card">
-                
-                <div className="card-body">
-                  <blockquote className="blockquote mb-0">
-                  <h4><FaLinkedin/> <a href="https://www.linkedin.com/in/vipin-kumar-382923248/" target='_blank'>Linkedin</a></h4>
-                  <h4><FaGithub/> <a href="https://github.com/vipinkumar66" target='_blank'>Github</a></h4>
-                  <h4><FaInstagram/> <a href="">Instagram</a></h4>
-                  <h4><FaYoutube/> <a href="https://www.youtube.com/channel/UCvSeIylWNRHZLP6rTdsCnNA" target='_blank'>Youtube</a></h4>
-                  </blockquote>
-                </div>
-            </div>
-          </div>
+      <div className='container about__container-social' data-aos="fade-left">
+        <div className='about__container-social__image'>
+          <img src="/images/social_media.svg" alt="social" />
+        </div>
+        <div className='about__container-content'>
+          <h4>The power of social media is that it allows you to connect with people on a deeper level, to build relationships, and to create a community around shared interests and values</h4>
+          <h1>Connect with ME...</h1>
+          <p><FaGithub/> <a href="https://github.com/vipinkumar66" target="_blank">Github</a></p>
+          <p><FaLinkedin/> <a href="https://www.linkedin.com/in/vipin-kumar-382923248/" target="_blank">Linkedin</a></p>
+          <p><FaInstagram/> <a href="">Instagram</a></p>
+          <p><FaYoutube/> <a href="https://www.youtube.com/channel/UCvSeIylWNRHZLP6rTdsCnNA" target="_blank">Youtube</a></p>
+        </div>
+      </div>
+      <div className='container about__resume'>
+        <div className='about__resume-content'>
+          <p>Want to have a look at my resume?</p>
+        </div>
+        <div className='about__resume-btn'>
+          <button type='text'><a href="/images/resume.pdf" target='_blank'>Download</a></button>
         </div>
       </div>
     </div>
